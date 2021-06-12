19b4e239135424192da5a08644eeeaae2e27b856
Rogelio Maldonado <rogermz.projects@gmail.com>
2021-06-12 13:03:19 -0500

    Ajuste de scripts

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 echo "Mostrando GET /"
-curl http://0.0.0.0:3000/
+curl http://localhost:3000/
 
 printf "\n\n"
 echo "Mostrando GET /saludo/<nombre>"
-curl http://0.0.0.0:3000/saludo/Roger
+curl http://localhost:3000/saludo/Roger
 
 printf "\n\n"
 echo "Fin de las pruebas... :)"
