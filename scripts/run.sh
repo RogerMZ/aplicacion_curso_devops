42c09c22bb2c8ea3b13c93e7fa9fe61382385052
Rogelio Maldonado <rogermz.projects@gmail.com>
2021-06-12 13:00:24 -0500

    Ajuste de scripts

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-#echo "docker run -itd --name test_api01 --rm rogermz/api_cdk_may:$1"
-docker run -itd --name test_api01 --rm rogermz/api_cdk_may:$1
+#echo "docker run -itd --name test_api01 -p 3000:3000--rm rogermz/api_cdk_may:$1"
+docker run -itd --name test_api01 -p 3000:3000 --rm rogermz/api_cdk_may:$1
 
